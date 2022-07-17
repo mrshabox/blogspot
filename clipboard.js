6b1418f0cd89595dd8c18967611fd97a71d2fd40
mrshabox <106615216+mrshabox@users.noreply.github.com>
2022-07-18 02:43:10 +0700

    Create clipboard.js

---
+++ clipboard.js
@@ -0,0 +1,9 @@
+function copy_data(containerid) {
+  var range = document.createRange();
+  range.selectNode(containerid); //changed here
+  window.getSelection().removeAllRanges(); 
+  window.getSelection().addRange(range); 
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+  alert("Đã sao chép! - Tgram.vn");
+}
